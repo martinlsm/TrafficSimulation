fcd1383ad66c5249c004ee77ea2a27b51f2b0db4
Martin Lindström <martin.lindstrom94@gmail.com>
2020-07-01 14:11:04 +0200

    Link in sfml library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
-#include <vector>
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-#include "car.h"
-
-int main(int argc, char *argv[])
+int main()
 {
-	CarBody car(1, 2);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
