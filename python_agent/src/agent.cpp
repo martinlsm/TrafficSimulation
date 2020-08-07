c93083cb61273e35fc073def893ac579818cb7e2
Martin Lindström <martin.lindstrom94@gmail.com>
2020-08-07 21:20:49 +0200

    Remove unused functions

--- python_agent/src/agent.cpp
+++ python_agent/src/agent.cpp
@@ -49,39 +49,4 @@ py::array_t<float> read_state(size_t car_index) {
 	return result;
 }
 
-py::array_t<double> add_arrays_1d(py::array_t<double>& input1, py::array_t<double>& input2) {
-
-	// Get info from input1, input2
-	py::buffer_info buf1 = input1.request();
-	py::buffer_info buf2 = input2.request();
-
-	if (buf1.ndim !=1 || buf2.ndim !=1)
-	{
-		throw std::runtime_error("Number of dimensions must be one");
-	}
-
-	if (buf1.size !=buf2.size)
-	{
-		throw std::runtime_error("Input shape must match");
-	}
-
-	//Apply resources
-	auto result = py::array_t<double>(buf1.size);
-	py::buffer_info buf3 = result.request();
-
-	//Obtain numpy.ndarray data pointer
-	double* ptr1 = (double*)buf1.ptr;
-	double* ptr2 = (double*)buf2.ptr;
-	double* ptr3 = (double*)buf3.ptr;
-
-	//Pointer visits numpy.ndarray
-	for (int i = 0; i < buf1.shape[0]; i++)
-	{
-		ptr3[i] = ptr1[i] + ptr2[i];
-	}
-
-	return result;
-
-}
-
 } // namespace agent
--- python_agent/src/main.cpp
+++ python_agent/src/main.cpp
@@ -5,44 +5,13 @@
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-    return i + j;
-}
-
 PYBIND11_MODULE(python_agent, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: python_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
-	 m.def("add_arrays_1d", &agent::add_arrays_1d);
-
-	m.def("load_traffic_environment", &agent::load_traffic_environment);
-	m.def("destination_count", &agent::destination_count);
-	m.def("car_count", &agent::car_count);
-	m.def("spawn_car", &agent::spawn_car);
-
-	m.def("read_state", &agent::read_state);
+    m.def("load_traffic_environment", &agent::load_traffic_environment);
+    m.def("destination_count", &agent::destination_count);
+    m.def("car_count", &agent::car_count);
+    m.def("spawn_car", &agent::spawn_car);
+    
+    m.def("read_state", &agent::read_state);
 
 #ifdef VERSION_INFO
     m.attr("__version__") = VERSION_INFO;
