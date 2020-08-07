207023623c2c247b533a0e02fd04d4edf6df439c
Martin Lindström <martin.lindstrom94@gmail.com>
2020-08-07 14:25:46 +0200

    Update configure script

--- configure.sh
+++ configure.sh
@@ -2,5 +2,5 @@
 THIS_DIR=$(pwd)
 
 # create symlinks for the agent to access cpp files
-ln -s ${THIS_DIR}/src python_agent/src/cpp_src
-ln -s ${THIS_DIR}/include python_agent/src/cpp_include
+ln -sf ${THIS_DIR}/src python_agent/src/cpp_src
+ln -sf ${THIS_DIR}/include python_agent/src/cpp_include
