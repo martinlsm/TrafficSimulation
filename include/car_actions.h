1c528f9159c81eb76c52670acbdbe29c7d01ac38
Martin Lindström <martin.lindstrom94@gmail.com>
2020-08-11 23:06:08 +0200

    Change var decls in car_actions.h to be extern

--- include/car_actions.h
+++ include/car_actions.h
@@ -6,18 +6,18 @@
 namespace traffic {
 
 typedef std::int64_t car_action;
-const car_action BRAKE_LIGHT       = 1L <<  0;
-const car_action BRAKE_MEDIUM      = 1L <<  1;
-const car_action BRAKE_HARD        = 1L <<  2;
-const car_action GAS_LIGHT         = 1L <<  3;
-const car_action GAS_MEDIUM        = 1L <<  4;
-const car_action GAS_HARD          = 1L <<  5;
-const car_action TURN_LEFT_LIGHT   = 1L <<  6;
-const car_action TURN_LEFT_MEDIUM  = 1L <<  7;
-const car_action TURN_LEFT_HARD    = 1L <<  8;
-const car_action TURN_RIGHT_LIGHT  = 1L <<  9;
-const car_action TURN_RIGHT_MEDIUM = 1L << 10;
-const car_action TURN_RIGHT_HARD   = 1L << 11;
+extern const car_action BRAKE_LIGHT;
+extern const car_action BRAKE_MEDIUM;
+extern const car_action BRAKE_HARD;
+extern const car_action GAS_LIGHT;
+extern const car_action GAS_MEDIUM;
+extern const car_action GAS_HARD;
+extern const car_action TURN_LEFT_LIGHT;
+extern const car_action TURN_LEFT_MEDIUM;
+extern const car_action TURN_LEFT_HARD;
+extern const car_action TURN_RIGHT_LIGHT;
+extern const car_action TURN_RIGHT_MEDIUM;
+extern const car_action TURN_RIGHT_HARD;
 
 void do_action(CarMechanics* car, const car_action action);
 
