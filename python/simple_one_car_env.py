1f2f052cccaf26c3b76fb205ff72cf98e1ba740d
Martin Lindström <martin.lindstrom94@gmail.com>
2020-08-22 15:00:40 +0200

    Rename module name in python file

--- python/simple_one_car_env.py
+++ python/simple_one_car_env.py
@@ -1,9 +1,9 @@
-import env_api as env
+import env_api
 import numpy as np
 import random
 
 TRAFFIC_ENV_ID = 2
-env.load_traffic_environment(TRAFFIC_ENV_ID)
+env_api.load_traffic_environment(TRAFFIC_ENV_ID)
 
 def state_dim_size():
     return 16
@@ -14,15 +14,15 @@ def action_dim_size():
 # this environment reduces the amount of possible actions to 5
 def __translate_action(action):
     if action == 0:
-        return env.car_action_do_nothing
+        return env_api.car_action_do_nothing
     elif action == 1:
-        return env.car_action_gas_medium
+        return env_api.car_action_gas_medium
     elif action == 2:
-        return env.car_action_brake_medium
+        return env_api.car_action_brake_medium
     elif action == 3:
-        return env.car_action_turn_left_hard
+        return env_api.car_action_turn_left_hard
     elif action == 4:
-        return env.car_action_turn_right_hard
+        return env_api.car_action_turn_right_hard
 
 def reset():
     global car_id
@@ -32,20 +32,20 @@ def reset():
     except NameError:
         pass
 
-    car_id = env.spawn_car(0, 1)
-    observation = env.read_state_sensors(car_id)
-    reward = env.get_reward_advanced(car_id)
-    done = env.in_terminal_state(car_id)
+    car_id = env_api.spawn_car(0, 1)
+    observation = env_api.read_state_sensors(car_id)
+    reward = env_api.get_reward_advanced(car_id)
+    done = env_api.in_terminal_state(car_id)
     return observation, reward, done
 
 def step(action):
     action = __translate_action(action)
-    env.do_action(car_id, action)
-    env.update()
+    env_api.do_action(car_id, action)
+    env_api.update()
 
-    observation = env.read_state_sensors(car_id)
-    reward = env.get_reward_advanced(car_id)
-    done = env.in_terminal_state(car_id)
+    observation = env_api.read_state_sensors(car_id)
+    reward = env_api.get_reward_advanced(car_id)
+    done = env_api.in_terminal_state(car_id)
 
     return observation, reward, done
 
